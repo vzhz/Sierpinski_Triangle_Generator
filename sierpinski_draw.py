26bb29077cd432e688fcd067dc76939f9421954f
vzhz <vehanus@gmail.com>
2016-10-21 22:07:18 -0400

    ft, fp working, s not : draw triangle now draws a triangle (modified from the pyramid script)

--- sierpinski_draw.py
+++ sierpinski_draw.py
@@ -7,22 +7,86 @@ from sys import version_info
 #def ask_desired_display():
 py3 = version_info[0] > 2
 if py3:
-    script_version = input("Would you like the full-speed (f) or the simplified version (s)?")
+    script_version = input("""Would you like the full-speed triangle (ft), full-speed pyramid (fp),
+                              or the simplified version (s)?""")
 else:
-    script_version = raw_input("Would you like the full-speed (f) or the simplified version (s)?")
+    script_version = raw_input("""Would you like the full-speed triangle (ft), full-speed pyramid (fp),
+                                  or the simplified version (s)?""")
 
 def run(script_version):
-    if script_version == "f":
+    if script_version == "ft":
+        print("Hello, triangle-lover!")
+        draw_triangle()
+        # eventually ask if they want to see others and rerun (but from inside that function?)
+    elif script_version == "fp":
         print("Hello, visualization-watcher!")
         draw_pyramids()
-    else:
+        # ask if they want to see other versions
+    elif script_version == "s":
         print("Hello, triangle-learner!")
         draw_pyramids_slow()
+        # ask if they want to see other versions (since they may have gotten confused and done the slow version and want to see fast now)
+    else:
+        print("Please run script again and choose a valid option.")
+        exit()
 
 
 
 def draw_triangle():
-    pass
+    ### window parameters for initial triangle visualization ###
+    width = 1000
+    height = 1000
+    color = pygame.Color(0, 255, 0)
+
+    ### additional parameters for slower, more informative visualization ###
+    highlight_P_1 = pygame.Color(255, 50, 50)
+    previous_P_0 = pygame.Color(50, 255, 50)
+    ABCD_color = pygame.Color(50, 50, 255)
+
+    ### create the window ###
+    pygame.init()
+    surface = pygame.display.set_mode((width, height))
+
+
+    def draw_pixel(x, y, count):
+        """Draws each pixel in the full-speed case"""
+        surface.fill(color, ((x, y), (1, 1)))
+        if count%100==0:
+            pygame.display.flip()
+
+    def random_row():
+        return random.randint(0, height)
+
+    def random_col():
+        return random.randint(0, width)
+
+    def midpoint(row1, row2, col1, col2):
+        rowm = (row1+row2)/2
+        colm = (col1+col2)/2
+        return (rowm, colm)
+
+    def random_point():
+        return random_row(), random_col()
+
+    P = (random_row(), random_col())
+    varibles = [random_point() for i in range(3)]
+    i = random.randint(0,2)
+
+    #def draw_either(chosen_one, P, draw_pixel)
+    count = 0
+    while count <= 100000:
+        i = (i + random.randint(0,2))%3
+        chosen_one = varibles[i]
+        a_row = chosen_one[0]
+        a_col = chosen_one[1]
+        b_row = P[0]
+        b_col = P[1]
+        draw_pixel(b_row, b_col, count)
+        P = (midpoint(a_row, b_row, a_col, b_col))
+        count += 1
+
+    pygame.event.wait()
+    pygame.event.wait()
 
 def draw_pyramids():
     ### window parameters for initial triangle visualization ###
@@ -90,26 +154,27 @@ def draw_pyramids_slow():
     pygame.init()
     surface = pygame.display.set_mode((width, height)) # creates a window
 
-    # def draw_pixel(x1, y1, x2, y2, x3, y3, count): #add count if you want to flip only occasionally
-    #     surface.fill(previous_P_0, ((x1, y1), (3, 3)))
-    #     #if count%1==0:
-    #     surface.fill(chosen_ABCD_0, ((x2, y2), (3, 3)))
-    #     pygame.draw.line(surface, (255, 50, 50), (x1, y1), (x2, y2), 1)
-    #     surface.fill(highlight_P_1, ((x3, y3), (3, 3)))
-    #     pygame.display.flip()
-    #     time.sleep(0.5)
-    #     pygame.draw.line(surface, (0, 0, 0), (x1, y1), (x2, y2), 1)
-    #     pygame.display.flip()
-    #     time.sleep(0.5)
-    # def draw_pixel_ABCD_1(x1, y1, x2, y2, count):
-    #     surface.fill(previous_P_0, ((x1, y1), (3, 3)))
-    #     surface.fill(chosen_ABCD_0, ((x2, y2), (3, 3)))
-    #     pygame.draw.line(surface, (255, 50, 50), (x1, y1), (x2, y2), 1)
-    #     pygame.display.flip()
-    #     time.sleep(0.5)
-    #     pygame.draw.line(surface, (0, 0, 0), (x1, y1), (x2, y2), 1)
-    #     pygame.display.flip()
-    #     time.sleep(0.5)
+    def draw_pixel(x1, y1, x2, y2, x3, y3, count): #add count if you want to flip only occasionally
+        surface.fill(previous_P_0, ((x1, y1), (3, 3)))
+        #if count%1==0:
+        surface.fill(chosen_ABCD_0, ((x2, y2), (3, 3)))
+        pygame.draw.line(surface, (255, 50, 50), (x1, y1), (x2, y2), 1)
+        surface.fill(highlight_P_1, ((x3, y3), (3, 3)))
+        pygame.display.flip()
+        time.sleep(0.5)
+        pygame.draw.line(surface, (0, 0, 0), (x1, y1), (x2, y2), 1)
+        pygame.display.flip()
+        time.sleep(0.5)
+
+    def draw_pixel_ABCD_1(x1, y1, x2, y2, count):
+        surface.fill(previous_P_0, ((x1, y1), (3, 3)))
+        surface.fill(chosen_ABCD_0, ((x2, y2), (3, 3)))
+        pygame.draw.line(surface, (255, 50, 50), (x1, y1), (x2, y2), 1)
+        pygame.display.flip()
+        time.sleep(0.5)
+        pygame.draw.line(surface, (0, 0, 0), (x1, y1), (x2, y2), 1)
+        pygame.display.flip()
+        time.sleep(0.5)
 
     #how to test if pixel cluster is always offset the same way
 
